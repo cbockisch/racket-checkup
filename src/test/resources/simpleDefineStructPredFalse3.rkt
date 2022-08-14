abfe49c072ebdf78f5e43a4b53790084eaa6fefa
NickLowack <lowack@students.uni-marburg.de>
2022-08-14 15:53:16 +0200

    forgot resources

---
+++ src/test/resources/simpleDefineStructPredFalse3.rkt
@@ -0,0 +1,6 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname simpleDefineStructPredFalse3) (read-case-sensitive #t) (teachpacks ((lib "universe.rkt" "teachpack" "2htdp") (lib "image.rkt" "teachpack" "2htdp") (lib "abstraction.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "universe.rkt" "teachpack" "2htdp") (lib "image.rkt" "teachpack" "2htdp") (lib "abstraction.rkt" "teachpack" "2htdp")) #f)))
+(define-struct fahrrad (reifen farbe))
+
+(fahrrad? (make-fahrrad 10 (make-fahrrad 10 101 10)))
